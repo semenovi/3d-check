d88d9e2494460040b6f15150e635562374860b12
semenovi <t79601712352@gmail.com>
2020-03-12 01:51:02 +0300

    binary input and output

--- main.cpp
+++ main.cpp
@@ -1,18 +1,3 @@
-//
-// Book:      OpenCL(R) Programming Guide
-// Authors:   Aaftab Munshi, Benedict Gaster, Timothy Mattson, James Fung, Dan Ginsburg
-// ISBN-10:   0-321-74964-2
-// ISBN-13:   978-0-321-74964-2
-// Publisher: Addison-Wesley Professional
-// URLs:      http://safari.informit.com/9780132488006/
-//            http://www.openclprogrammingguide.com
-//
-
-// HelloWorld.cpp
-//
-//    This is a simple example that demonstrates basic OpenCL setup and
-//    use.
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -30,41 +15,44 @@
 const int ARRAY_SIZE = 1000;
 
 
-cl_int *readGraph(const char *fileName)
+void write_model(std::string file_name, cl_int n, cl_int *adjacency_matrix)
 {
-    cl_int errNum;
-    char *currentString = new char[32];
-    std::ifstream inputFile(fileName, std::ios::in);
+    std::ofstream output_stream(file_name, std::ios::binary);
 
-    if (!inputFile.is_open())
-    {
-        std::cerr << "Failed to open file for reading: " << fileName << std::endl;
-        return NULL;
+    if (!output_stream.is_open()) {
+        std::cerr << "Failed to open file to write: " << file_name << std::endl;
+        return;
     }
 
-    std::ostringstream oss;
+    output_stream << n << ' ';
 
-    inputFile.getline(currentString, 1);
+    for (cl_int i = 0; i < n; i++)
+    {
+        output_stream << adjacency_matrix[i] << ' ';
+    }
+}
+
+void read_model(std::string file_name)
+{
+    std::ifstream input_stream(file_name, std::ios::binary);
+
+    if (!input_stream.is_open())
+    {
+        std::cerr << "Failed to open file for reading: " << file_name << std::endl;
+        return;
+    }
+
+    cl_int n = 0;
+    input_stream >> n;
+        
+    cl_int* adjacency_matrix = new cl_int[n];
+    cl_int i = 0;
+    while (input_stream.good()) {
+        input_stream >> adjacency_matrix[i];
+        i++;
+    }
     
-
-    cl_int* g = new cl_int[];
-
-    oss << inputFile.rdbuf();
-
-    std::string inputStdStr = oss.str();
-    const char* srcStr = inputStdStr.c_str();
-    /*
-    program = clCreateProgramWithSource(context, 1,
-        (const char**)&srcStr,
-        NULL, NULL);
-    if (program == NULL)
-    {
-        std::cerr << "Failed to create CL program from source." << std::endl;
-        return NULL;
-    }
-    */
-
-    return g;
+    return;
 }
 
 ///
@@ -265,112 +253,21 @@ void Cleanup(cl_context context, cl_command_queue commandQueue,
 //
 int main(int argc, char** argv)
 {
-    readGraph("3d_model.in");
-    /*
-    cl_context context = 0;
-    cl_command_queue commandQueue = 0;
-    cl_program program = 0;
-    cl_device_id device = 0;
-    cl_kernel kernel = 0;
-    cl_mem memObjects[3] = { 0, 0, 0 };
-    cl_int errNum;
-
-    // Create an OpenCL context on first available platform
-    context = CreateContext();
-    if (context == NULL)
+    std::string input_file("3d_model.in");
+    cl_int* adjacency_matrix = new cl_int[64]
     {
-        std::cerr << "Failed to create OpenCL context." << std::endl;
-        return 1;
-    }
+            0, 3, 5, 4, 3, 4, 0, 0,
+            3, 0, 4, 0, 0, 3, 4, 0,
+            5, 4, 0, 3, 0, 0, 3, 4,
+            4, 0, 3, 0, 4, 0, 0, 3,
+            3, 0, 0, 4, 0, 3, 5, 4,
+            4, 3, 0, 0, 3, 0, 4, 0,
+            0, 4, 3, 0, 5, 4, 0, 3,
+            0, 0, 4, 3, 4, 0, 3, 0
+    };
+    write_model("3d_model.in", 64, adjacency_matrix);
+    
+    read_model("3d_model.in");
 
-    // Create a command-queue on the first device available
-    // on the created context
-    commandQueue = CreateCommandQueue(context, &device);
-    if (commandQueue == NULL)
-    {
-        Cleanup(context, commandQueue, program, kernel, memObjects);
-        return 1;
-    }
-
-    // Create OpenCL program from HelloWorld.cl kernel source
-    program = CreateProgram(context, device, "kernel.cl");
-    if (program == NULL)
-    {
-        Cleanup(context, commandQueue, program, kernel, memObjects);
-        return 1;
-    }
-
-    // Create OpenCL kernel
-    kernel = clCreateKernel(program, "hello_kernel", NULL);
-    if (kernel == NULL)
-    {
-        std::cerr << "Failed to create kernel" << std::endl;
-        Cleanup(context, commandQueue, program, kernel, memObjects);
-        return 1;
-    }
-
-    // Create memory objects that will be used as arguments to
-    // kernel.  First create host memory arrays that will be
-    // used to store the arguments to the kernel
-    float result[ARRAY_SIZE];
-    float a[ARRAY_SIZE];
-    float b[ARRAY_SIZE];
-    for (int i = 0; i < ARRAY_SIZE; i++)
-    {
-        a[i] = (float)i;
-        b[i] = (float)(i * 2);
-    }
-
-    if (!CreateMemObjects(context, memObjects, a, b))
-    {
-        Cleanup(context, commandQueue, program, kernel, memObjects);
-        return 1;
-    }
-
-    // Set the kernel arguments (result, a, b)
-    errNum = clSetKernelArg(kernel, 0, sizeof(cl_mem), &memObjects[0]);
-    errNum |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &memObjects[1]);
-    errNum |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &memObjects[2]);
-    if (errNum != CL_SUCCESS)
-    {
-        std::cerr << "Error setting kernel arguments." << std::endl;
-        Cleanup(context, commandQueue, program, kernel, memObjects);
-        return 1;
-    }
-
-    size_t globalWorkSize[1] = { ARRAY_SIZE };
-    size_t localWorkSize[1] = { 1 };
-
-    // Queue the kernel up for execution across the array
-    errNum = clEnqueueNDRangeKernel(commandQueue, kernel, 1, NULL,
-                                    globalWorkSize, localWorkSize,
-                                    0, NULL, NULL);
-    if (errNum != CL_SUCCESS)
-    {
-        std::cerr << "Error queuing kernel for execution." << std::endl;
-        Cleanup(context, commandQueue, program, kernel, memObjects);
-        return 1;
-    }
-
-    // Read the output buffer back to the Host
-    errNum = clEnqueueReadBuffer(commandQueue, memObjects[2], CL_TRUE,
-                                 0, ARRAY_SIZE * sizeof(float), result,
-                                 0, NULL, NULL);
-    if (errNum != CL_SUCCESS)
-    {
-        std::cerr << "Error reading result buffer." << std::endl;
-        Cleanup(context, commandQueue, program, kernel, memObjects);
-        return 1;
-    }
-
-    // Output the result buffer
-    for (int i = 0; i < ARRAY_SIZE; i++)
-    {
-        std::cout << result[i] << " ";
-    }
-    std::cout << std::endl;
-    std::cout << "Executed program succesfully." << std::endl;
-    Cleanup(context, commandQueue, program, kernel, memObjects);
-    */
     return 0;
 }
